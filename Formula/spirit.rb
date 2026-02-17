ac7f05dcb388fb3a3b84504688e55b9532e77692
root <root@vultr.guest>
2026-02-17 10:04:35 +0000

    Add SPIRIT v0.1.0 formula

--- Formula/spirit.rb
+++ Formula/spirit.rb
@@ -1,5 +1,3 @@
-# Formula for SPIRIT - State Preservation & Identity Resurrection Infrastructure Tool
-# https://github.com/TheOrionAI/spirit
 class Spirit < Formula
   desc "CLI tool that preserves AI agent state and enables resurrection on new servers"
   homepage "https://github.com/TheOrionAI/spirit"
@@ -7,20 +5,22 @@ class Spirit < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/TheOrionAI/spirit/releases/download/v0.1.0/spirit_0.1.0_darwin_amd64.tar.gz"
       sha256 "f7e07571d91954a624c15f3d36df5b912a53b8bb91bac1ad7742d7e928c0747f"
-    else
+    end
+    on_arm do
       url "https://github.com/TheOrionAI/spirit/releases/download/v0.1.0/spirit_0.1.0_darwin_arm64.tar.gz"
       sha256 "52605eb15c229e253e9283db336a493805f7dd51ef80414858a47751f07b38a3"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/TheOrionAI/spirit/releases/download/v0.1.0/spirit_0.1.0_linux_amd64.tar.gz"
       sha256 "07e62d944ff2b70fe93fcded0f9ef737f1254ac2b0326e8b2a72119115509d42"
-    else
+    end
+    on_arm do
       url "https://github.com/TheOrionAI/spirit/releases/download/v0.1.0/spirit_0.1.0_linux_arm64.tar.gz"
       sha256 "2b8171e44c16bd20c67b9cfddc2e6540bdb87d874321ccf267361a979e9f4c7f"
     end
@@ -32,7 +32,7 @@ class Spirit < Formula
 
   def caveats
     <<~EOS
-      🌌 SPIRIT has been installed!
+      SPIRIT has been installed!
 
       Quick start:
         spirit init --name="my-agent" --emoji="🤖"
@@ -44,6 +44,6 @@ class Spirit < Formula
   end
 
   test do
-    assert_match "SPIRIT v0.1.0", shell_output("#{bin}/spirit --version")
+    system "#{bin}/spirit", "--version"
   end
 end
